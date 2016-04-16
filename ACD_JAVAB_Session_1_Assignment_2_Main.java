97e42f2d24a1862abe40544e7e45bdd6573d4cb2
krushnadeb <krushna_deb@rediffmail.com>
2016-04-16 19:40:43 +0530

    Added files via upload

---
+++ ACD_JAVAB_Session_1_Assignment_2_Main.java
@@ -0,0 +1,10 @@
+//Declare three integer variables (a,b,c) in a single declaration and simultaneously initialize b to 4.
+
+public class ACD_JAVAB_Session_1_Assignment_2_Main {
+
+	public static void main(String[] args) {	
+		int a,b=4,c;
+		System.out.println("b = "+b);			
+
+		}
+}
